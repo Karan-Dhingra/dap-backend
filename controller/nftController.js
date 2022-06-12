c3d39a9e9a8e21856a10efd6a85fa132e568ee51
karan-dhingra <dhingrakaran764@gmail.com>
2022-06-13 00:54:31 +0530

    update/delete nfts

--- controller/nftController.js
+++ controller/nftController.js
@@ -66,6 +66,117 @@ const createNft = async (req, res) => {
     }
 }
 
+const deleteNft = async (req, res) => {
+    try {
+        const nft = req.body
+        console.log(nft)
+        if (!nft._id) {
+            res.json({ status: 404, msg: 'Id is required!' })
+            return
+        }
+        if (!nft.owner) {
+            res.json({ status: 404, msg: 'Owner is required!' })
+            return
+        }
+
+        const ownerData = await User.findOne({ walletAddress: nft.owner })
+        if (ownerData) {
+            if (ownerData.isAdmin) {
+                const deleteNft = await Nft.deleteOne({ _id: nft._id })
+
+                res.json({
+                    status: 200,
+                    msg: 'Nft Deleted successfully!!',
+                    data: deleteNft,
+                })
+
+                return
+            } else {
+                res.json({ status: 404, msg: 'You are not authorized!' })
+                return
+            }
+        } else {
+            res.json({ status: 404, msg: 'Something went wrong!' })
+            return
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(501).json({ msg: err.toString() })
+    }
+}
+
+const updateNft = async (req, res) => {
+    try {
+        const nft = req.body
+        console.log(nft)
+        if (!nft._id) {
+            res.json({ status: 404, msg: 'Id is required!' })
+            return
+        }
+        if (!nft.name) {
+            res.json({ status: 404, msg: 'Name is required!' })
+            return
+        }
+        if (!nft.description) {
+            res.json({ status: 404, msg: 'Description is required!' })
+            return
+        }
+        if (!nft.price) {
+            res.json({ status: 404, msg: 'Price is required!' })
+            return
+        }
+        if (nft.nftType === undefined) {
+            res.json({ status: 404, msg: 'Nft Type is required!' })
+            return
+        }
+        if (!nft.nftImage) {
+            res.json({ status: 404, msg: 'Nft Image is required!' })
+            return
+        }
+        if (!nft.owner) {
+            res.json({ status: 404, msg: 'Owner is required!' })
+            return
+        }
+
+        const ownerData = await User.findOne({ walletAddress: nft.owner })
+        if (ownerData) {
+            if (ownerData.isAdmin) {
+                const updateNft = await Nft.updateOne(
+                    { _id: nft._id },
+                    {
+                        $set: {
+                            name: nft.name,
+                            description: nft.description,
+                            price: nft.price,
+                            nftType: nft.nftType,
+                            timer: nft.timer,
+                            nftImage: nft.nftImage,
+                            owner: nft.owner,
+                        },
+                    }
+                )
+
+                res.json({
+                    status: 200,
+                    msg: 'Nft Updated successfully!!',
+                    data: updateNft,
+                })
+
+                return
+            } else {
+                res.json({ status: 404, msg: 'You are not authorized!' })
+                return
+            }
+        } else {
+            res.json({ status: 404, msg: 'Something went wrong!' })
+            return
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(501).json({ msg: err.toString() })
+    }
+}
+
 const getAllNft = async (req, res) => {
     try {
         const nfts = await Nft.find({})
@@ -76,4 +187,4 @@ const getAllNft = async (req, res) => {
     }
 }
 
-module.exports = { createNft, getAllNft }
+module.exports = { createNft, getAllNft, updateNft, deleteNft }
--- routes/nftRoute.js
+++ routes/nftRoute.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const cors = require('cors')
-const { createNft, getAllNft } = require('../controller/nftController')
+const {
+    createNft,
+    getAllNft,
+    updateNft,
+    deleteNft,
+} = require('../controller/nftController')
 
 router.get('/', cors(), getAllNft)
 router.post('/create', cors(), createNft)
+router.post('/update', cors(), updateNft)
+router.post('/delete', cors(), deleteNft)
 module.exports = router
